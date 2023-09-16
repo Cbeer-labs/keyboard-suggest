7aa7e05fe930c77f20019dc57f90642037c661e0
Anton Klimanov <klimanov625@outlook.com>
2023-09-16 11:10:44 +0300

    [scripts] migrate to meson

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+restore_dir=$(pwd)
+cd $(git rev-parse --show-toplevel)
+
 set -e
+export CTEST_OUTPUT_ON_FAILURE=true
 export GTEST_COLOR=1
-CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
-CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
-CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/gcc-7-cxx17-pic.cmake'"
-CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
-cmake -H. -B_builds $CMAKE_OPTS
-cmake --build _builds
-cmake --build _builds --target test -- ARGS="--verbose"
+
+meson setup _builds
+meson compile -C _builds tests
+./_builds/tests/tests --verbose
+
+rm -r ./_builds
+
+cd "$restore_dir"
