80331b6ad8c07182128126372ad756c267f3ddca
Anton Klimanov <klimanov625@outlook.com>
2023-12-11 19:38:43 +0300

    [scripts] absolute dist path in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ restore_dir=$(pwd)
 cd $(git rev-parse --show-toplevel)
 
 set -e
-meson setup _builds --prefix dist
+meson setup _builds --prefix "$(pwd)/dist"
 meson install -C _builds
 
 cd "$restore_dir"
